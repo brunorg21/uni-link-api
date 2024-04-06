5fb40de455a35b6b8e8193e5ed50aed84d19abcf
brunorg21 <rbrunog21@gmail.com>
2024-04-06 10:49:00 -0300

    feat: add delete subject use case

---
+++ src/domain/use-cases/delete-subject.ts
@@ -0,0 +1,20 @@
+import { ResourceNotFoundError } from "@/errors/resource-not-found-error";
+import { SubjectRepository } from "../repositories/subject-repository";
+
+interface DeleteSubjectUseCaseRequest {
+  subjectId: string;
+}
+
+export class DeleteSubjectUseCase {
+  constructor(private subjectRepository: SubjectRepository) {}
+
+  async execute({ subjectId }: DeleteSubjectUseCaseRequest): Promise<void> {
+    const subject = await this.subjectRepository.findById(subjectId);
+
+    if (!subject) {
+      throw new ResourceNotFoundError();
+    }
+
+    await this.subjectRepository.delete(subjectId);
+  }
+}
